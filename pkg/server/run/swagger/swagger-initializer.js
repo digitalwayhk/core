3d9b3f78c03c5830203d0ce0f7376a0016185245
Vincent <vincent@dalveytech.com>
2023-06-07 17:29:18 +0800

    update swagger url

--- pkg/server/run/swagger/swagger-initializer.js
+++ pkg/server/run/swagger/swagger-initializer.js
@@ -1,19 +1,18 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
-  var host="http://"+window.location.host+'/api/openapi';
+  var domain = window.location.protocol + "//" + window.location.hostname;
+  if (window.location.port != "") {
+    domain = domain + ":" + window.location.port;
+  }
+  var host = domain + "/api/openapi";
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: host,//https://petstore.swagger.io/v2/swagger.json
-    dom_id: '#swagger-ui',
+    url: host, //https://petstore.swagger.io/v2/swagger.json
+    dom_id: "#swagger-ui",
     deepLinking: true,
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
   });
 
   //</editor-fold>
