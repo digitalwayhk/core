178de2a9ca3828e3b051769ddb4163e52e2c957f
Vincent <vincent@dalveytech.com>
2023-07-12 16:43:01 +0800

    update logs

--- pkg/server/config/serverconfig.go
+++ pkg/server/config/serverconfig.go
@@ -72,7 +72,7 @@ func NewServiceDefaultConfig(servicename string, port int) *ServerConfig {
 	ip := utils.GetLocalIP()
 	con.Log.ServiceName = servicename + "-" + ip
 	con.Log.KeepDays = 10
-	con.Log.Mode = "file"
+	//con.Log.Mode = "file"
 	//con.Log.Path = "logs/" + servicename
 	con.RunIp = ip
 	con.Auth.AccessSecret = uuid.Must(uuid.NewV4()).String()
